8dec27fd9e32f6b7f427da56ec825a4dd67122f7
kasyanov_ks <fierymail@list.ru>
2020-01-17 03:30:51 +0300

    Добавил миграцию с тестовыми пользователями и их ролями.

--- src/main/resources/db/migration/V2__Add_Admin.sql
+++ src/main/resources/db/migration/V2__Add_Admin.sql
@@ -1,3 +1,3 @@
 insert into usr (id, username, password, creation_date)
-    values (1,'admin','admin',localtimestamp)
+    values ('56a6f09f-455c-457a-ac22-e0bb8b0874f7','admin','admin',localtimestamp);
 
